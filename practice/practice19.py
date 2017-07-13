b2d245e974fc7203e1ac5e808cb41d7f3b24ca43
repaocha <10290804941@qq.com>
2017-07-13 15:55:42 +0800

    python

---
+++ practice/practice19.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+
+#一个数如果恰好它的因子之和，这个数就称为“完数”。例如6=1+2+3.编程找出1000以内的所有完数。
+for i in range(1, 1001):
+    sum = 0
+    k=[]
+    for j in range(1, i):
+        if i % j == 0:
+            sum += j
+            k.append(j)    
+    if sum == i:
+        print(i)
---
+++ practice/practice20.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#　-*-coding:UTF-8 -*-
+
+#一球从100米高度自由落下，每次落地后反跳回原高度的一半，求它在第10次落地时，共经过多少米？
+#第10次反弹多高？
+   
+tour=[]
+height=[]
+
+hei=100.0
+time=10
+for i in range(1,time+1):
+    if i==1:
+        tour.append(hei)
+    else:
+        tour.append(2*hei)
+    hei /= 2
+    height.append(hei)
+print('总长度：tour={0}'.format(sum(tour)))
+print('第10次反弹高度：%s' % (min(height)))
