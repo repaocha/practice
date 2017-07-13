c0cc5ac74b3d34c1fb3328a552d7092770d70faa
repaocha <10290804941@qq.com>
2017-07-13 18:21:04 +0800

    python

---
+++ practice/practice27.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+
+#利用递归函数调用方式，将所输入的5个字符，以相反的顺序打印出来。
+
+def output(s,l):
+    if l==0:
+       return
+    print (s[l-1])
+    output(s,l-1)
+    
+s=raw_input('请输入一串字符:')
+l=len(s)
+output(s,l)
