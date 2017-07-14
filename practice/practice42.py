a0e6c1bff7317ab17a4c3c163bed2ebded1d98a0
repaocha <10290804941@qq.com>
2017-07-14 18:18:03 +0800

    python

---
+++ practice/practice42.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+from _ast import Num
+
+#学习使用auto定义变量的用法。
+
+num=2
+def autofunc():
+    num=1
+    print 'internal block num=%d' % num
+    num+=1
+for i in range(4):
+    print 'The num=%d' % num
+    num+=1
+    autofunc()
---
+++ practice/practice45.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+
+#计算1到100之和。
+
+num=0
+for i in range(1,101):
+    num+=i
+print num
